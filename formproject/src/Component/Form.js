980fa671ee73ca0aa3b4b5169ddb94d0c0834c23
Senthilprabhu1 <senthilprabhu12@gmail.com>
2018-12-04 11:19:53 +0530

    form

---
+++ formproject/src/Component/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="Form">
+        
+      </div>
+    );
+  }
+}
+
+export default Form;
